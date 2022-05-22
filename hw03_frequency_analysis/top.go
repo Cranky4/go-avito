59844f601e36401b7391e098086eaa457dbf3306
cranky4 <sylvl0n@yandex.ru>
2022-05-22 13:58:48 +0300

    fix error, add own tests

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -11,14 +11,18 @@ type WordCounter = struct {
 }
 
 func Top10(text string) []string {
+	const maxResults = 10
+
 	words := strings.Fields(text)
 
 	if len(words) == 0 {
 		return words
 	}
 
+	// дробим строку на подстроки
 	wordsWithCountMap := make(map[string]WordCounter)
 
+	// накручиваем мапу структур для подсчета, используя ключ мапы, как признак уже найденного слова
 	for _, word := range words {
 		var currentCounter WordCounter
 		if _, ok := wordsWithCountMap[word]; ok {
@@ -32,20 +36,26 @@ func Top10(text string) []string {
 		wordsWithCountMap[word] = currentCounter
 	}
 
+	// превращаем мапу в слайс, чтобы сортировать
 	wordsWithCountSlice := make([]WordCounter, 0, len(wordsWithCountMap))
 	for _, counter := range wordsWithCountMap {
 		wordsWithCountSlice = append(wordsWithCountSlice, counter)
 	}
 
+	// сортируем по убыванию количества
 	sort.Slice(wordsWithCountSlice, func(i, j int) bool {
 		if wordsWithCountSlice[i].Count == wordsWithCountSlice[j].Count {
 			return wordsWithCountSlice[i].Word < wordsWithCountSlice[j].Word
 		}
 		return wordsWithCountSlice[i].Count > wordsWithCountSlice[j].Count
 	})
-	wordsWithCountSlice = wordsWithCountSlice[0:10]
 
-	result := make([]string, 0, 10)
+	// готовим результат
+	if len(wordsWithCountSlice) >= maxResults {
+		wordsWithCountSlice = wordsWithCountSlice[0:maxResults]
+	}
+
+	result := make([]string, 0, maxResults)
 	for _, counter := range wordsWithCountSlice {
 		result = append(result, counter.Word)
 	}
