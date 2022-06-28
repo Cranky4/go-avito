6a868d98bb72620e32ad71d7970d4024ed5ab1f8
cranky4 <sylvl0n@yandex.ru>
2022-06-28 16:22:43 +0300

    hw06 base completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,27 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for i := 0; i < len(stages); i++ {
+		in = stages[i](proxyInput(in, done))
+	}
+
+	return in
+}
+
+func proxyInput(in, done In) Bi {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+		for v := range in {
+			select {
+			case <-done:
+				return
+			default:
+				out <- v
+			}
+		}
+	}()
+
+	return out
 }
